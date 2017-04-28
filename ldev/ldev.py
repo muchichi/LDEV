4b2ff51d33d254b6e5831995f3cc9283ad033ae2
Mussie Nadew <muchichi@gmail.com>
2017-04-27 20:51:27 -0400

    Initial commit

---
+++ ldev/ldev.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 27 20:06:54 2017
+
+@author: mussie
+
+Common data problems
+    - Inconsistent column names
+    - Missing Data
+    - Outliers
+    - Duplicate rows
+    - Untidy (transformation issue)
+    - Need to process columns
+    - Column types can signal unexpected data values
+"""
+
+def main():
+    pass
+
+# Laod data
+def load(filename):
+    pass
+
+def diagnose():
+    pass
+
+def explore():
+    pass
+
+def visualize():
+    pass
+
+
+
+if __name__ == '__main__':main()
+
+
